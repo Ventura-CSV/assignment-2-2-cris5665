f706c12d595b9a9e8095d97e8dff3fcafc94fdd1
“Cristian <ctoledo5665@gmail.comgit config --global user.name cris5665git config --global user.email ctoledo5665@gmail.com>
2023-08-24 21:05:02 -0700

    main.py

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ def main():
     Use the same variables: celcius fahrenheit 
     ##################################################
     """
-
+    celsius= int(input("Enter the temparature in Celsius: "))
+    print(celsius)
+    fahrenheit= (float(celsius) * 1.8) +32
+    print (format (fahrenheit, ".2f"))
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celcius, fahrenheit
+    return celsius, fahrenheit
 
 
 if __name__ == '__main__':
